3c92ceeb18465a037222c431286082f09a81f020
ds-shin <dssgo2010@gmail.com>
2020-12-30 11:07:10 +0900

    환경세팅 완료

---
+++ sql/create.sql
@@ -0,0 +1,25 @@
+-- 유저이름@아이피주소
+create user 'cos'@'%' identified by 'cos1234';
+
+-- ON DB이름.테이블명
+-- TO 유저이름@아이피주소
+GRANT ALL PRIVILEGES ON *.* TO 'cos'@'%';
+CREATE DATABASE blog CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
+use blog;
+
+show variables where Variable_name like '%dir';
+
+/*
+my.ini 추가후 재시작
+[client]
+default-character-set=utf8
+
+[mysql]
+default-character-set=utf8
+
+[mysqld]
+collation-server = utf8_unicode_ci
+init-connect='SET NAMES utf8'
+init_connect='SET collation_connection = utf8_general_ci'
+character-set-server=utf8
+*/
